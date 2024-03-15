282c64a192fb4fd8a4fcfcbb241492709e29436a
carmineiaucci <iauccicarmine006@gmail.com>
2024-03-15 17:22:29 +0100

    query per giocatore umano

--- Forza4/Game/game_logic.py
+++ Forza4/Game/game_logic.py
@@ -54,3 +54,33 @@ class ForzaQuattroGame:
 
         # applica il complemento a 1 per il cambio di turno
         self.turn = ~self.turn
+
+    def query_player(self):
+        print("\nMossa del giocatore...")
+        column = None
+        # ciclo per ottenere la colonna in cui si vuole mettere la pedina
+        while column is None:
+            # column = input('Your move identify column [0-6]? ')
+            try:
+                # column = int(column)
+                column = get_human_interaction(self.board, self.game_instance)  # ottiene la colonna attraverso
+                # l'interfaccia grafica
+                # controlla se la colonna è valida (compresa tra 0 e 6)
+                if not 0 <= column <= 6:
+                    raise ValueError
+                # controlla se la colonna è piena (tramite una maschera bit a bit)
+                if self.current_state.game_position & (1 << (7 * column + 5)):
+                    raise IndexError
+            except (ValueError, IndexError):
+                print("Invalid move. Try again...")
+                column = None
+
+        # esecuzione della mossa
+        drop_human_token(self.board, column, self.game_instance)
+
+        # calcola la nuova posizione e la nuova posizione di gioco
+        new_position, new_game_position = make_move(self.current_state.player_position,
+                                                    self.current_state.game_position, column)
+
+        # aggiorna lo stato corrente del gioco con la nuova posizione di gioco e incrementa la profondità di uno.
+        self.current_state = State(self.current_state.ai_position, new_game_position, self.current_state.depth + 1)
